98f125300de1949cc0889260aa6896e728a1864f
WORLDSKILLS <juandiego1918@gmail.com>
2018-08-31 10:33:11 -0500

    juan

--- app/src/main/java/com/example/worldskills/turisapp/utilidades/Conexion.java
+++ app/src/main/java/com/example/worldskills/turisapp/utilidades/Conexion.java
@@ -1,4 +1,22 @@
 package com.example.worldskills.turisapp.utilidades;
 
-public class Conexion {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class Conexion extends SQLiteOpenHelper {
+    public Conexion(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(Utilidades.CREAR_TABLA);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + Utilidades.NOMBRE_TABLA);
+        onCreate(db);
+    }
 }
---
+++ app/src/main/java/com/example/worldskills/turisapp/utilidades/Utilidades.java
@@ -0,0 +1,16 @@
+package com.example.worldskills.turisapp.utilidades;
+
+public class Utilidades {
+
+
+    public static final String NOMBRE_TABLA = "LUGARES";
+    public static final String NOMBRE_LUGAR = "NOMBRE";
+    public static final String DESCRIPCION_CORTA = "CORTA";
+    public static final String DESCRIPCION_LARGA = "LARGA";
+    public static final String UBICACION = "UBICACION";
+    public static final String LATITUD = "LATITUD";
+    public static final String LONGITUD = "LONGITUD";
+
+    public static final String CREAR_TABLA = "CREATE TABLE " + NOMBRE_TABLA + " ( " + NOMBRE_LUGAR + " VARCHAR , " + DESCRIPCION_CORTA + " VARCHAR, " +
+            DESCRIPCION_LARGA + " VARCHAR, " + UBICACION + " VARCHAR, " + LATITUD + " VARCHAR, " + LONGITUD + " VARCHAR);";
+}
